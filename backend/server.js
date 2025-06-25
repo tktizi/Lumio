0a305d7a664eae1e1838146bcd11ce4a8e2865cf
tktizi <maks.krish7@gmail.com>
2025-06-25 15:44:41 +0300

    Fetching Movies from API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
@@ -11,8 +13,11 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); // allow parse req.body 
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
+
 
 app.listen(PORT, () => {
 	console.log("Server started at http://localhost:" + PORT);
     connectDB();
 });
+
