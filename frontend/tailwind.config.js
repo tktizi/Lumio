a33d00f542b190fe6a5ca332f7eaf0b2af2579e3
tktizi <maks.krish7@gmail.com>
2025-06-29 17:33:04 +0300

    Home Screen

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -12,5 +13,5 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [tailwindScrollbarHide],
 }
