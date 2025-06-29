b0b01c6b26b622f10bc594d762dc1cd771510fd2
tktizi <maks.krish7@gmail.com>
2025-06-29 22:50:28 +0300

    Watch Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import Footer from "./components/Footer";
+import WatchPage from "./pages/WatchPage";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
@@ -33,6 +34,7 @@ function App() {
 				<Route path='/' element={<HomePage />} />
 				<Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
 				<Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
+				<Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
 			</Routes>
 
 			<Footer/>
